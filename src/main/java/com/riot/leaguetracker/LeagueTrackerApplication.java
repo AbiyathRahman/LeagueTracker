10b9bdc66fbc102b6cd387592370c827e55ab787
Abiyath Rahman <abiyathrahman@mail.weber.edu>
2025-07-28 11:34:38 -0600

    Initial commit

---
+++ src/main/java/com/riot/leaguetracker/LeagueTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.riot.leaguetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeagueTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/riot/leaguetracker/LeagueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riot.leaguetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
